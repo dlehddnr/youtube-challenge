5128233720831000217724792effe2caa7c3b411
mac <dy95032@gmail.com>
2019-06-28 07:42:28 +0900

    Done

--- src/movieController.js
+++ src/movieController.js
@@ -25,12 +25,25 @@ export const movieDetail = async (req, res) => {
 };
 
 export const filterMovie = async (req, res) => {
+  const {
+    query: { rating, year }
+  } = req;
   try {
-    const movieList = await getMovieByMinimumRating(8);
-    if (movieList) {
-      res.render("movies", { movieList });
+    if (rating) {
+      const movieList = await getMovieByMinimumRating(rating);
+      if (movieList) {
+        res.render("movies", { movieList, searchingBy: `Rating : ${rating}` });
+      }
+    } else if (year) {
+      const movieList = await getMovieByMinimumYear(year);
+      if (movieList) {
+        res.render("movies", { movieList, searchingBy: `Year : ${year}` });
+      }
+    } else {
+      res.render("404");
     }
   } catch (error) {
+    console.log(error);
     res.render("404");
   }
 };
